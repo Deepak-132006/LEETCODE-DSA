4c68831ecc79c00c4942c95095262b4c6e1238f6
Deepak-132006 <deepakn132006@gmail.com>
2025-12-07 10:02:47 +0530

    Solved Leetcode #125

--- LeetCode/ValidPalindrome.java
+++ LeetCode/ValidPalindrome.java
@@ -1,5 +1,34 @@
 package LeetCode;
 
 public class ValidPalindrome {
-    
+    public void validate(String s) {
+
+        s = "A man, a plan, a canal: Panama";
+        StringBuffer valid = new StringBuffer();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                ch = Character.toLowerCase(ch);
+                valid.append(ch);
+            }
+        }
+        int left = 0;
+        int right = valid.length() - 1;
+
+        while (left < right) {
+            if (valid.charAt(left) != valid.charAt(right)) {
+                System.out.print("Not Palindrome!");
+                return;
+            }
+            left++;
+            right--;
+        }
+        System.out.print("Palindrome");
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome obj = new ValidPalindrome();
+        obj.validate(null);
+    }
 }
