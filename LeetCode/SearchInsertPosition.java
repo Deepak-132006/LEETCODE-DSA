d5537e213e77716e7ebee2db353ca25286f37afb
Deepak-132006 <deepakn132006@gmail.com>
2025-12-10 11:41:41 +0530

    Solved Leetcode #35

--- LeetCode/SearchInsertPosition.java
+++ LeetCode/SearchInsertPosition.java
@@ -23,7 +23,7 @@ public class SearchInsertPosition {
             }
         }
         System.out.print("Not Found");
-        return -1;
+        return left;
     }
 
     public static void main(String[] args) {
