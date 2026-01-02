72ab42ac20094736fff727bb1d5f474d438c952c
Deepak-132006 <deepakn132006@gmail.com>
2026-01-02 09:54:08 +0530

    Removing Duplicates - Two Pointers

--- TwoPointers/RemoveDuplicates.java
+++ TwoPointers/RemoveDuplicates.java
@@ -1,5 +1,22 @@
 package TwoPointers;
 
+import java.util.Arrays;
+
 public class RemoveDuplicates {
-    
+    public static int[] Removeduplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return Arrays.copyOf(nums, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 4, 4, 5, 6, 6, 7, 9 };
+        RemoveDuplicates obj = new RemoveDuplicates();
+        System.out.println(Arrays.toString(obj.Removeduplicates(nums)));
+    }
 }
