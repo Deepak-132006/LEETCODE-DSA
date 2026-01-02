a7bba183351cdd57f62ae3bcc4ae33dc1dffb66f
Deepak-132006 <deepakn132006@gmail.com>
2026-01-02 10:23:49 +0530

    Solved Leetcode #283

---
+++ LeetCode/MoveZeroes.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void zeroes(int[] nums) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        while (i < nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1, 0, 3, 12 };
+        zeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
