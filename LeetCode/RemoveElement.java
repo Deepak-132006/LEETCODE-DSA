c8f078e0e36a129587fb10e7a2c0a5d47ae12d3d
Deepak-132006 <deepakn132006@gmail.com>
2026-01-05 09:43:28 +0530

    Solved Leetcode #27

---
+++ LeetCode/RemoveElement.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement obj = new RemoveElement();
+        int[] nums = { 0,1,2,2,3,0,4,2};
+        System.out.println(obj.removeElement(nums, 2));
+    }
+}
