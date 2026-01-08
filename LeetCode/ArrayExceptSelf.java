c7a0b7faff386fa0f766bd656160e071d7cc4fe3
Deepak-132006 <deepakn132006@gmail.com>
2026-01-08 18:07:18 +0530

    Solved Leetcode #238

--- LeetCode/ArrayExceptSelf.java
+++ LeetCode/ArrayExceptSelf.java
@@ -5,23 +5,24 @@ import java.util.Arrays;
 public class ArrayExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
         int[] productArray = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            int product = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    product *= nums[j];
-                }
-            }
-            productArray[i] = product;
+        productArray[0] = 1;
+        for (int i = 1; i < n; i++) {
+            productArray[i] = productArray[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for(int i = n - 1; i >= 0; i--){
+            productArray[i] *= right;
+            right *= nums[i];
         }
         return productArray;
     }
 
     public static void main(String[] args) {
         ArrayExceptSelf obj = new ArrayExceptSelf();
-        int[] nums = { 1, 2, 3, 4 };
+        int[] nums = { -1,1,0,-3,3 };
         System.out.println(Arrays.toString(obj.productExceptSelf(nums)));
     }
 }
