77b89c2007ac5cbb9432da2268be2dc5999e5cd2
Deepak-132006 <deepakn132006@gmail.com>
2026-01-25 13:33:06 +0530

    Recursion Power of a Number

---
+++ Recursion/PowerXN.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class PowerXN {
+    public double power(double x, int n){
+        
+        if(n == 0) return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;            
+        } else {
+            return x * half * half;
+        }
+    }
+    public static void main(String[] args) {
+        PowerXN obj = new PowerXN();
+        double x = 2;
+        int n = 5;
+        System.out.println(obj.power(x, n));
+    }
+}
