1f0300d27cfe5abd7bfb7c78f377dc0c2b24a7e1
Deepak-132006 <deepakn132006@gmail.com>
2026-01-27 21:24:17 +0530

    Solved Leetcode #509

---
+++ LeetCode/Fibonacci.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class Fibonacci {
+    public int fab(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fab(n - 2) + fab(n - 1);
+    }
+    public static void main(String[] args) {
+        Fibonacci obj = new Fibonacci();
+        int n = 2;
+        System.out.println(obj.fab(n));
+    }
+}
