61d7098ee067489cfe139ff2a31849b103cd0113
Deepak-132006 <deepakn132006@gmail.com>
2026-01-27 21:23:10 +0530

    Recursion - Reverse Number

---
+++ Recursion/ReverseNumber.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class ReverseNumber {
+    public int reverse(int n, int rev){
+        if(n == 0) return rev;
+        return reverse(n / 10, rev * 10 + n % 10);
+    }
+    public static void main(String[] args) {
+        ReverseNumber obj = new ReverseNumber();
+        int n = 1234;
+        System.out.println(obj.reverse(n, 0));
+    }
+}
