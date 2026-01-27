778b3dc7d7c0ed1394768cbada9385ecc4798090
Deepak-132006 <deepakn132006@gmail.com>
2026-01-27 21:07:58 +0530

    Recursion - Fibonacci Series

---
+++ Recursion/FiboSeries.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class FiboSeries {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        FiboSeries obj = new FiboSeries();
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(obj.fib(i));
+        }
+    }
+}
