29a765e1d007870effcd899642fd2275f787c631
Deepak-132006 <deepakn132006@gmail.com>
2026-01-31 15:09:46 +0530

    MoveZeros Done

---
+++ Practice/MoveZeros.java
@@ -0,0 +1,26 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != 0){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        while (i < nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        MoveZeros obj = new MoveZeros();
+        int[] nums = { 0, 1, 0, 3, 12 };
+        obj.moveZeroes(nums);
+    }
+}
