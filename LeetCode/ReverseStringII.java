a0b04c60d9f71e4ab0ebc05b6cac789003ff7ba8
Deepak-132006 <deepakn132006@gmail.com>
2026-02-18 20:58:37 +0530

    Solved Leetcode #541

---
+++ LeetCode/ReverseStringII.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+public class ReverseStringII {
+    private void reverse(char arr[], int left, int right){
+        while(left < right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public String reverseStr(String s, int k) {
+      char[] arr = s.toCharArray();
+      for(int i = 0; i < arr.length; i += 2 * k ){
+        int start = i;
+        int end = Math.min(i + k - 1, arr.length - 1);
+        reverse(arr, start, end);
+      }
+      return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdefg";
+        int k = 2;
+        ReverseStringII obj = new ReverseStringII();
+        System.out.println(obj.reverseStr(s, k));
+    }
+
+}
