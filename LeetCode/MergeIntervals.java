01572b976391c843afa23ae9076375eb243424e8
Deepak-132006 <deepakn132006@gmail.com>
2026-03-05 21:23:37 +0530

    Solved Leetcode #56

--- LeetCode/MergeIntervals.java
+++ LeetCode/MergeIntervals.java
@@ -1,12 +1,32 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
 
+        int[] current = intervals[0];
+        result.add(current);
+
+        for (int i = 0; i < intervals.length; i++) {
+            int[] next = intervals[i];
+
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                current = next;
+                result.add(current);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
     }
 
     public static void main(String[] args) {
-
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+       System.out.println(Arrays.deepToString(new MergeIntervals().merge(intervals)));
     }
-
 }
