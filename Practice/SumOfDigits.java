917b473fbeb4d340722338b43b0a1296214ee168
Deepak-132006 <deepakn132006@gmail.com>
2026-04-02 10:47:53 +0530

    Level 2 - P4

---
+++ Practice/SumOfDigits.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class SumOfDigits {
+    public static int sumDigits(int n){
+        if(n == 0) return 0;
+        return n % 10 + sumDigits(n / 10);
+    }
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(new SumOfDigits().sumDigits(n));
+    }
+}
