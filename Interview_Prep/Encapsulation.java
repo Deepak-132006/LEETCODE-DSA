e69e667f4db8537e9007792fa04eee44dd4a371d
Deepak-132006 <deepakn132006@gmail.com>
2026-04-05 17:55:56 +0530

    Implemented Encapsulation

--- Interview_Prep/Encapsulation.java
+++ Interview_Prep/Encapsulation.java
@@ -13,6 +13,21 @@ public class Encapsulation {
     }
 
     public int getAge(){
-        return  
+        return age;
+    }
+
+    public void setAge(int age){
+        if(age >= 18 && age > 0){
+            this.age = age;
+        } else {
+            System.out.println("Invalid Age !!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Encapsulation e = new Encapsulation();
+        e.setAge(21);
+        e.setName("Deepak");
+       System.out.println(e.getAge() + e.getName());       
     }
 }
