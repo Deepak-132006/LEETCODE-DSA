42fd158e2fce61e945f09046f568e7aec5e411fe
Deepak-132006 <deepakn132006@gmail.com>
2026-07-29 15:20:45 +0530

    Solved Leetcode #242

--- LeetCode/ValidAnagram.java
+++ LeetCode/ValidAnagram.java
@@ -1,30 +1,31 @@
 package LeetCode;
 
+import java.util.HashMap;
+
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        s = s.toLowerCase();
-        t = t.toLowerCase();
-
-        int[] count = new int[26];
         if (s.length() != t.length())
             return false;
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
+        }
 
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
+        for (int i = 0; i < t.length(); i++) {
+            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
         }
-        for (char c : t.toCharArray()) {
-            count[c - 'a']--;
+
+        if (mapS.equals(mapT)) {
+            return true;
         }
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
         ValidAnagram obj = new ValidAnagram();
-        System.out.print(obj.isAnagram("anaGram", "nagaram"));
+        System.out.print(obj.isAnagram(s, t));
     }
 }
