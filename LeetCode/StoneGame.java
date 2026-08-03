14055f70da0064adad893784c714c1998fa37bcd
Deepak-132006 <deepakn132006@gmail.com>
2026-08-03 21:09:52 +0530

    Solved Leetcode #877

---
+++ LeetCode/StoneGame.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class StoneGame {
+        public boolean stoneGame(int[] piles) {
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] piles = {5, 3, 4, 5};
+        System.out.println(new StoneGame().stoneGame(piles));
+    }
+}
+
+
+// Input: piles = [5,3,4,5]
+// Output: true
