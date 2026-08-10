f5436dacc19d1fc3dbeb8652a29106372ebc6f26
Deepak-132006 <deepakn132006@gmail.com>
2026-08-10 21:43:12 +0530

    Explored Stack Operations

---
+++ Stack/Practice.java
@@ -0,0 +1,14 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Practice {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        stack.add(4);
+        System.out.println(stack.get(1));
+    }
+}
