0b7338faa980a775ddce61c2e960514f8ee95c46
Deepak-132006 <deepakn132006@gmail.com>
2026-08-15 15:28:44 +0530

    Floyd's Cycle Detection

--- Patterns/FloydCycleDetection.java
+++ Patterns/FloydCycleDetection.java
@@ -1,7 +1,44 @@
 package Patterns;
 
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 public class FloydCycleDetection {
+
+    public static boolean hasCycle(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node fourth = new Node(40);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second;
+
+        boolean result = new FloydCycleDetection().hasCycle(head);
+        System.out.println(result);
     }
 }
