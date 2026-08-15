ecb3d3ae83d65f03f41cb97068154ccd916795c1
Deepak-132006 <deepakn132006@gmail.com>
2026-08-15 14:41:34 +0530

    Learnt LinkedList

---
+++ LinkedList/Practice.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Practice {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        head.next = second;
+        second.next = third;
+
+        Node current = head;
+
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
