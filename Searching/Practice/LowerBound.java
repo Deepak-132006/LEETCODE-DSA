480db41543102a90158b08e020ae01df74369e6a
Deepak-132006 <deepakn132006@gmail.com>
2026-08-16 21:49:16 +0530

    Solved Lower Bound

---
+++ Searching/Practice/LowerBound.java
@@ -0,0 +1,26 @@
+package Searching.Practice;
+
+public class LowerBound {
+    public int lowerBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, index = 0;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target){
+                index = mid;
+                right = mid - 1;
+            }
+            if(nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 4, 4, 6, 8 };
+        int target = 0;
+        System.out.println(new LowerBound().lowerBound(nums, target));
+    }
+}
