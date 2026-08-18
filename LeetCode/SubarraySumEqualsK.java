45d1f6631c11561875d0e6f0588e8f496e34bbe5
Deepak-132006 <deepakn132006@gmail.com>
2026-08-18 22:04:42 +0530

    Solved Leetcode #560

--- LeetCode/SubarraySumEqualsK.java
+++ LeetCode/SubarraySumEqualsK.java
@@ -1,16 +1,28 @@
 package LeetCode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int sum = 0, result = 0;
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            sum += num;
+            int need = sum - k;
+            if(map.containsKey(need)){
+                count += map.get(need);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, -1, 2, 3 };
-        int count = 0;
-        System.out.println(new SubarraySumEqualsK().subarraySum(nums, count));
+        int[] nums = { 1, 2, 3 };
+        int k = 3;
+        System.out.println(new SubarraySumEqualsK().subarraySum(nums, k));
     }
 }
