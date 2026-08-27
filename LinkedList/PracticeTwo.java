91296f9c84822bddc60193295b862fe761902d04
Deepak-132006 <deepakn132006@gmail.com>
2026-08-27 22:03:14 +0530

    Linkedlist - insert, delete, display

---
+++ LinkedList/PracticeTwo.java
@@ -0,0 +1,97 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    void insertAtPosition(int data, int position) {
+        Node newNode = new Node(data);
+
+        if(position == 0){
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+
+        for(int i = 0; i < position - 1; i++){
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+    void deleteAtPosition(int position){
+        if(head == null){
+            return;
+        }
+
+        if(position == 0){
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+
+        for(int i = 0; i < position - 1; i++){
+            current = current.next;
+        }
+        current.next = current.next.next;
+    }
+}
+
+public class PracticeTwo {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+
+        list.insertAtBeginning(0);
+        
+        list.insertAtPosition(5, 1);
+        list.display();
+        list.deleteAtPosition(0);
+        list.display();
+    }
+}
