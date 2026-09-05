309ecf703b4c13942e8ce217baf4ebe0ec45be4a
Deepak-132006 <deepakn132006@gmail.com>
2026-09-05 21:30:20 +0530

    Bitwise...

---
+++ Interview_Prep/Bitwise/BitPractice.java
@@ -0,0 +1,16 @@
+package Interview_Prep.Bitwise;
+
+public class BitPractice {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+
+        int and = a & b;
+        int or = a | b;
+        int xor = a ^ b;
+
+        System.out.println(and);
+        System.out.println(or);
+        System.out.println(xor);
+    }
+}
